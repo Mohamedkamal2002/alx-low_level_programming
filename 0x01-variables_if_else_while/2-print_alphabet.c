c989c86740fc2786cd4578ca55f4cf17b25ff0d5
MhmdKamal <MhmdKamal@gmail.com>
2023-03-17 13:33:11 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the alphabet in lower case
+ *
+ * Description: printing the alphabet using for loop
+ * Return: always return 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for(alphabet = 'a'; alphabet <= z; alphabeta++)
+		printf("%c", alphabet);
+	return (0);
+}
