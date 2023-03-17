f077e4ba3d577598f351481688f9c77586544871
MhmdKamal <MhmdKamal@gmail.com>
2023-03-17 14:26:11 -0700

    numbers from 1 to 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,9 +9,11 @@
 int main(void)
 {
 	char SD_number;
+	char newline;
 
+	newline = "\n";
 	for (SD_number = 0; SD_number <= 9; SD_number++)
 		putchar(SD_number);
-	putchar("\n");
+	putchar(newline);
 	return (0);
 }
