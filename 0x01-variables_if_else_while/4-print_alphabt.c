1ee39c7529118470b16ce0bac5e357ee7fcddb72
MhmdKamal <MhmdKamal@gmail.com>
2023-03-17 14:06:12 -0700

    alphabet without eq

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the alphabet in lower case
+ *
+ * Description: printing the alphabet using for loop
+ * Return: always return 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		if (alphabet != 'e' || alphabet != 'q')
+			putchar(alphabet);
+	printf("\n");
+	return (0);
+}
