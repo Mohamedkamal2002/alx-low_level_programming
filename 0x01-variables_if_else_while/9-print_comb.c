8228c0adc9193c98c40d314b691e8fb467d0888e
MhmdKamal <MhmdKamal@gmail.com>
2023-03-17 15:20:43 -0700

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the single digit numbers
+ *
+ * Description: printing the single digit numbers using for loop
+ * Return: always return 0
+ */
+int main(void)
+{
+	char SD_number;
+	char newline;
+	char comma;
+	char space;
+
+	comma = ',';
+	space = ' ';
+	for (SD_number = '0'; SD_number <= '8'; SD_number++)
+	{
+		putchar(SD_number);
+		purchar(comma);
+		putchar(space;
+	}
+	SD_number++;
+	putchar(SD_number);
+	return (0);
+}
