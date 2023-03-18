531a3261776ee00b086c820032e72795f3d6b9e2
MhmdKamal <MhmdKamal@gmail.com>
2023-03-17 17:10:09 -0700

    3 digit combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,20 +18,20 @@ int main(void)
 	comma = ',';
 	space = ' ';
 	nwline = '\n';
-	for(TH_number = '0'; TH_number <= '9'; TH_number++)
-	for (SD_number = TH_number + 1; SD_number <= '9'; SD_number++)
-		for (FD_number = SD_number + 1; FD_number <= '9'; FD_number++)
-		{
-			putchar(TH_number);
-			putchar(SD_number);
-			putchar(FD_number);
-			if (TH_number != '7')
+	for (TH_number = '0'; TH_number <= '9'; TH_number++)
+		for (SD_number = TH_number + 1; SD_number <= '9'; SD_number++)
+			for (FD_number = SD_number + 1; FD_number <= '9'; FD_number++)
 			{
-				putchar(comma);
-				putchar(space);
-			}
+				putchar(TH_number);
+				putchar(SD_number);
+				putchar(FD_number);
+				if (TH_number != '7')
+				{
+					putchar(comma);
+					putchar(space);
+				}
 
-		}
+			}
 	putchar(nwline);
 	return (0);
 }
