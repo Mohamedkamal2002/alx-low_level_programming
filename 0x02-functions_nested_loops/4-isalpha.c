e99ab959c5363053f9caf31d53efc6049e09aebe
MhmdKamal <MhmdKamal@gmail.com>
2023-03-21 11:22:12 -0700

    find  letters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+/**
+ * _islower - check alphabet
+ * @c: character passed to be checked
+ *
+ * Description: This function check if the paramater passed is letter using ASCII table
+ * Return: return 1 if the @c is letter and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'A' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
