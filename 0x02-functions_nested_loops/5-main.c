0bd45ac00327368d6c27a8f5cff1bea11cf52d4b
MhmdKamal <MhmdKamal@gmail.com>
2023-03-21 15:22:00 -0700

    check the sign

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,15 +1,20 @@
-#include "main.h"
 /**
  * main - call the print_call function
  *
  * Description: call the print_call function and check the return value for it.
  * Return: Always 0
  */
+#include <limits.h>
+#include "main.h"
 
 int main(void)
 {
 	int r;
-	
+
 	r = print_sign(98);
+	if (r < 0)
+		r *= -1;
+	_putchar(r + '0');
+	_putchar('\n');
 	return (0);
 }
