6282652a02c2524186d85feba5ea1f573cb3a096
MhmdKamal <MhmdKamal@gmail.com>
2023-03-21 15:09:42 -0700

    check the sign

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -11,17 +11,5 @@ int main(void)
 	int r;
 	
 	r = print_sign(98);
-	/*r = print_sign(-4);*/
-	if (r < 0)
-	{
-		_putchar('-');
-		r *= -1;
-	}
-	else if (r ==0)
-		_putchar('0');
-	else
-		_putchar('+');
-	_putchar(r + '0');
-	_putchar('\n');
 	return (0);
 }
