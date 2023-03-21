45a6e38efc2d1bc0a0d8e46aae1ab2f8e45ad64c
MhmdKamal <MhmdKamal@gmail.com>
2023-03-21 16:15:40 -0700

    last digit value

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - call the function
+ *
+ * Description: ~
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int r;
+
+	r = print_last_digit(98);
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+/**
+ * print_last_digit - return the last decimal digit
+ * @n: the whole number
+ *
+ * Description:~
+ * Return: the last digit
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
