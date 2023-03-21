229b1e44dcb00511e18285c68adfaa1c7219fa78
MhmdKamal <MhmdKamal@gmail.com>
2023-03-21 12:45:32 -0700

    check the sign

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * main - call the print_call function
+ *
+ * Description: call the print_call function and check the return value for it.
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int r;
+	r = print_sign(98);
+	switch (r)
+	{
+		case 1:
+			{
+				_putchar('+');
+				_putchar(',');
+				_putchar(' ');
+				_putchar('1');
+			}
+			case -1:
+			{
+				_putchar('-');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(r + '0');
+			}
+			case 0:
+			{
+				_putchar('0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0');
+			
+			}
+	}
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+/**
+ * print_sign - check the sign
+ * @n: the number to be checked
+ * 
+ * Description: check if the number is positive , negative or zero
+ * Return: 1 for positive , 0 for zero ; -1 for negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
