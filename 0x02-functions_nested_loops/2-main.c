b73ec59806eae5af33ff617edba42a269b9ce900
MhmdKamal <MhmdKamal@gmail.com>
2023-03-21 06:09:43 -0700

    printing alphabet lowercase 10 times

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * main -  calls print_alphabet_x10 function.
+ * 
+ * Description: calls print_alphabet_x10 function.
+ * Return: Always  return 0
+ */
+int main(viod)
+{
+        print_alphabet_x10(); /*function call*/
+        return 0;
+}
+
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,7 @@
 #include<stdio.h>
+#include "main.h"
 /*This program prints alphabets in lowercases 10 times.*/
 
-void print_alphabet_x10(void); /*prototyping*/
-
-/**
- * main -  calls print_alphabet_x10 function.
- * 
- * Description: calls print_alphabet_x10 function.
- * Return: Always  return 0
- */
-int main(viod)
-{
-	print_alphabet_x10(); /*function call*/
-	return 0;
-}
 
 /**
  * print_alphabet_x10 - This function prints alphabet in lowercase 10 times.
@@ -28,6 +16,9 @@ void print_alphabet_x10(void)
 	char i;
 
 	for (i = 0 ; i < 10 ; i++)
-		for (s = 'a' ; s <= 'z' ; s++ )
+	{	for (s = 'a' ; s <= 'z' ; s++ )
 			printf("%c",s);
+		printf("\n");
+	}
+
 }
