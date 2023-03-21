dae14a1f3e516a6ef3aad09014da23f69ceca232
MhmdKamal <MhmdKamal@gmail.com>
2023-03-21 16:03:04 -0700

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,15 @@
 /**
  * _abs - absolute value
+ * @n: the original value
  *
  * Description:~
  * Return: absolute value of n
  */
 int _abs(int n)
 {
-	if (n >= 0)
-		return (n);
-	else
-	{
+	if (n < 0)
 		n *= -1;
-		return (n);
-	}
+	return (n);
+	
 
 }
