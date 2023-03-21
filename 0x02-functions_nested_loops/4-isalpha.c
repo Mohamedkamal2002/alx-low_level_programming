3206ba90be7a5e88ce16ef681409f0a763516920
MhmdKamal <MhmdKamal@gmail.com>
2023-03-21 11:43:14 -0700

    find  letters

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
-/*iii*
+/**
  * _islower - check alphabet
- *
  * @c: character passed to be checked
+ *
  * Description: This function check if the paramater passed is
- * letter using ASCII table
+ *  letter using ASCII table
  * Return: return 1 if the @c is letter and 0 otherwise
  */
 
