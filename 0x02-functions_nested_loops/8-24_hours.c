cc65caa9ad6f00d201f8eda587531de71e2befae
MhmdKamal <MhmdKamal@gmail.com>
2023-03-21 18:41:59 -0700

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,11 +14,11 @@ void jack_bauer(void)
 	char hour10;
 
 	for (hour10 = '0' ; hour10 < '3' ; hour10++)
-		for (hour = '0' ; hour <= '9' ; hour++)
+		for (hour = '0' ; hour <= '6' ; hour++)
 			for (min10 = '0' ; min10 < '6' ; min10++)
 				for (min = '0' ; min <= '9' ; min++)
 				{
-					if (hour10 == 2 && hour == 4)
+					if (hour10 == 2 && hour  > 3)
 						break;
 					_putchar(hour10);
 					_putchar(hour);
