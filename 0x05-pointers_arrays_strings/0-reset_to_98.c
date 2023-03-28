e305b1a80ce2a6bb7c8584060148ce377c316a98
MhmdKamal <MhmdKamal@gmail.com>
2023-03-28 13:41:03 -0700

    Update pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update pointer value
+ * @n : the pointer should be updated
+ *
+ * Description:  update pointer value to 98
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
