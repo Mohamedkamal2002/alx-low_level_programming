86e56ddb8f1439fdfd060dfc910043bed2fc41a2
MhmdKamal <MhmdKamal@gmail.com>
2023-03-28 14:45:56 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - printing string
+ * @str: str to be printed
+ *
+ * Description: printing string
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	while(*str != '\0')
+		_putc((*str)++);
+}
