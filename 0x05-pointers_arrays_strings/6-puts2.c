9e6f28a9390ee32f83f6e85cba5c618929fcedaa
MhmdKamal <MhmdKamal@gmail.com>
2023-03-28 18:43:18 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _puts2 - printing every other char. of string
+ * @str: str to be printed
+ *
+ * Description: if str "fsgfg" it prints "fgg"
+ * Return: nothing
+ */
+
+void _puts2(char *str)
+{
+	char temp;
+
+	while (*str != '\0')
+	{
+		temp = *str;
+		str++;
+		if(str != '\0')
+			str++;
+		_putchar(temp);
+	}
+	_putchar('\n');
+}
