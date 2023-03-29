9a6f7ecc478fc9e0ce0a2960044f85f3eb97c299
MhmdKamal <MhmdKamal@gmail.com>
2023-03-28 20:01:45 -0700

    srcopy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * _strcpy - string copy
+ * @dest: destination
+ * @src: source
+ *
+ * Description: ~
+ * Return: pointer to char.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+
+	len = 0;
+	while (*src != '\0')
+	{
+		len++;
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*(dest + 1) = '\0';
+	dest = dest - len;
+	return dest;
+
+}
