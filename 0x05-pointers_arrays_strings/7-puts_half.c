f71c38b4ec634c3c0b6058e44c4f916acda2b734
MhmdKamal <MhmdKamal@gmail.com>
2023-03-28 19:03:51 -0700

    print half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * puts_half - print half string
- * @s: the string to be divided and printed
+ * @str: the string to be divided and printed
  *
  * Description: ~
  * Return: nothing
@@ -19,7 +19,7 @@ void puts_half(char *str)
 		str++;
 	}
 	str = str - length;
-	length = length / 2 ;
+	length = length / 2;
 	for (i = 0; i <  length ; i--)
 	{
 		temp = *str;
