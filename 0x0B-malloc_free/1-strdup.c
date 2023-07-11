48e64b31a7764193f969af987e76fb8dd5ae1c7e
Mhmdkamal <MhmdKamal@gmail.com>
2023-07-11 14:31:50 -0700

    copy string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,19 +10,19 @@
 char *_strdup(char *str)
 {
 	char *str_cp;
-	int i,l;
+	int i , l;
 
 	if (str == NULL)
 		return (NULL);
 	i = 1;
-	while(str[i]!= '\0')
+	while (str[i] != '\0')
 		i++;
 	str_cp = malloc(i + 1);
 	if (str_cp == NULL)
 		return (NULL);
 	for (l = 0 ; l <= i ; l++)
 		str_cp[l] = str[l];
-	if(str_cp == NULL)
+	if (str_cp == NULL)
 		return (NULL);
 	return (str_cp);
 }
