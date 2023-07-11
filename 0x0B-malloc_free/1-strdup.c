41ac2ac38319974e44329668b1e746439d319765
Mhmdkamal <MhmdKamal@gmail.com>
2023-07-11 05:07:45 -0700

    duplicate array

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,13 @@
+#include "stdlib.h"
+#include "string.h"
+char *_strdup(char *str)
+{
+	char *str_cp;
+
+	if (str == NULL)
+		return (NULL);
+	str_cp = malloc(sizeof(str));
+	/*if(str_cp == NULL)
+		return (NULL);*/
+	return (str_cp);
+}
