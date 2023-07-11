c3875ea4b55987debdaa981771b55c663e68f113
Mhmdkamal <MhmdKamal@gmail.com>
2023-07-11 14:02:07 -0700

    copy string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,12 +1,16 @@
 #include "stdlib.h"
-#include "string.h"
 char *_strdup(char *str)
 {
 	char *str_cp;
+	int i,l;
 
 	if (str == NULL)
 		return (NULL);
-	str_cp = malloc(sizeof(str));
+	i = 1;
+	while(str[i]!= '\0')
+		i++
+	for (l = 0 ; l <= i ; l++)
+		str_cp[l] = str[l];
 	if(str_cp == NULL)
 		return (NULL);
 	strcpy(str_cp,str);
