772a0f404f5491ab274c8998bfb77d8661a073eb
Mhmdkamal <Mhmd@gmail.com>
2023-08-17 15:15:41 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -25,8 +25,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(args, int));
 		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
-		else
-			printf("\n");
 	}
 	va_end(args);
 }
